95ed26aa47fd100a68e3e2fa57995af49eecdc9c
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-12-28 11:56:35 -0500

    feat: add git ignore

--- .history/vite.config_20231227194423.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite"
-import dlight from "../../packages/tools/vite-plugin-dlight/dist"
-
-export default defineConfig({
-  plugins: [dlight({ files: "**/*.view.{ts,js}" })],
-})
--- .history/vite.config_20231228115012.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite"
-import dlight from "vite-plugin-dlight"
-
-export default defineConfig({
-  plugins: [dlight({ files: "**/*.view.{ts,js}" })],
-})
